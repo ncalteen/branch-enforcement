cc6bd67371ac6873821aea95f65f3d142b444592
Nick Alteen <ncalteen@github.com>
2023-08-11 14:30:56 -0400

    Tests package-check workflow

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ export async function run(): Promise<string> {
     // Log the inputs.
     core.info(`Policy: ${inputPolicy.replace(/\n/g, ' | ')}`)
     core.info(`Head: ${headRef}`)
-    core.info(`Base: ${baseRef}`)
+    core.info(`Base: ${baseRef}`) //Comment for testing
 
     // Parse and validate the policy.
     const parsedPolicy: Policy = await parseBranchPolicy(core, inputPolicy)
