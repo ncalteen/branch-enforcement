0bfeb19a04082f77b550f6fd476b2c0ad72b8397
Nick Alteen <ncalteen@github.com>
2024-12-16 15:03:26 -0500

    Upgrade ESLint and Rollup

--- dist/utils/create-regex.d.ts
+++ dist/utils/create-regex.d.ts
@@ -1,4 +1,7 @@
 /**
- * Create a regex from a string
+ * Creates a regex from a string.
+ *
+ * @param ref The string to create a regex from.
+ * @returns A regex created from the string.
  */
 export declare function createRegex(ref: string): Promise<RegExp>;
